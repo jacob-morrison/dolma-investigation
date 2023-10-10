e03efb473dad38bb05fc4adad28a903c4f70541f
jacob-morrison <jacobm@allenai.org>
2023-10-10 11:45:45 -0700

    initial file

---
+++ dolma-language-investigation.py
@@ -0,0 +1,14 @@
+import dolma
+
+fake_document_text = 'this is my full document text that I\'m testing things out with'
+fake_document = dolma.core.data_types.Document(
+    'my brain',
+    0.1,
+    'fake-document-0.1',
+    fake_document_text
+)
+fake_text_slice = dolma.core.data_types.TextSlice(fake_document.text, 0, 16)
+
+print('hello')
+tagger = dolma.language.Cld3LanguageTagger()
+print(tagger.predict(doc = fake_document))
